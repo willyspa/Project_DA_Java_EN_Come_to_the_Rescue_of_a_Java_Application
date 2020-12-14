382555eebee43de831bbb159fd2547df3f352774
willyspa <armed@hotmail.fr>
2020-12-14 19:04:23 +0100

    symptoms with number of occurence

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,73 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Enumeration;
 import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+
 	
 	public static void main(String args[]) throws Exception {
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("C:\\Users\\armed\\git\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\src\\com\\hemebiotech\\analytics\\symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+		List<String> symptoms = new ArrayList<String>();
+		Hashtable<String,Integer> symptomsMap = new Hashtable<String,Integer>();
 
+		String key;
+
+		while (line != null) {
+
+			symptoms.add(line);
+			java.util.Collections.sort(symptoms);
 			line = reader.readLine();	// get another symptom
 		}
 		
-		// next generate output
+		for(String symptom:symptoms) {
+			
+			System.out.println(symptom);
+			symptomsMap.put(symptom,0);
+		}
+	
+
+	    for(String symptom:symptoms) {
+	    	symptomsMap.put(symptom,symptomsMap.get(symptom)+1);
+	    }
+
+	    
+		Enumeration<String> symptomsName = symptomsMap.keys();
+		
+	    while(symptomsName.hasMoreElements()) {
+	    	key = (String)symptomsName.nextElement();
+	    	System.out.println(key+" : "+symptomsMap.get(key));
+
+	  	}
+
+	    
+
+	    
+
+	    
+	
+	
+	 
+
+	 
+		
+		
+		
+		
+		/* next generate output
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
+		*/
 	}
 }
